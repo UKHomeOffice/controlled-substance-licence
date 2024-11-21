fa0c0984dc930f923bf5be274028dcac4d521104
dk4g <157156245+dk4g@users.noreply.github.com>
2024-11-21 15:54:32 +0000

    Bugfix: CSL-52 - Optional field throws error with and without data in the field

--- apps/precursor-chemicals/fields/index.js
+++ apps/precursor-chemicals/fields/index.js
@@ -2,47 +2,47 @@ const dateComponent = require('hof').components.date;
 
 module.exports = {
   'company-name': {
-    mixins: ['input-text'],
+    mixin: ['input-text'],
     validate: ['required', 'notUrl', { type: 'minlength', arguments: 2 }, { type: 'maxlength', arguments: 200 }],
     className: ['govuk-input', 'govuk-!-width-two-thirds']
   },
   'company-number': {
-    mixins: ['input-text'],
-    validate: [ 'notUrl', { type: 'regex', arguments: /^[A-Za-z]{1,2}\d{8,12}$/}],
+    mixin: ['input-text'],
+    validate: [], // additional validation rules added in custom-validation.js
     className: ['govuk-input', 'govuk-!-width-two-thirds']
   },
   telephone: {
-    mixins: ['input-text'],
+    mixin: ['input-text'],
     validate: ['required'], // additional validation covered in custom-validation.js
     className: ['govuk-input', 'govuk-!-width-two-thirds']
   },
   email: {
-    mixins: ['input-text'],
+    mixin: ['input-text'],
     validate: ['required', 'email'],
     className: ['govuk-input', 'govuk-!-width-two-thirds']
   },
   'website-url': {
-    mixins: ['input-text'],
+    mixin: ['input-text'],
     validate: ['url', { type: 'maxlength', arguments: 250 }],
     className: ['govuk-input', 'govuk-!-width-two-thirds']
   },
   'licence-holder-address-line-1': {
-    mixins: ['input-text'],
+    mixin: ['input-text'],
     validate: ['required', 'notUrl', { type: 'maxlength', arguments: 250 }],
     className: ['govuk-input', 'govuk-!-width-two-thirds']
   },
   'licence-holder-address-line-2': {
-    mixins: ['input-text'],
+    mixin: ['input-text'],
     validate: ['notUrl', { type: 'maxlength', arguments: 250 }],
     className: ['govuk-input', 'govuk-!-width-two-thirds']
   },
   'licence-holder-town-or-city': {
-    mixins: ['input-text'],
+    mixin: ['input-text'],
     validate: ['required', 'notUrl', { type: 'maxlength', arguments: 250 }],
     className: ['govuk-input', 'govuk-!-width-two-thirds']
   },
   'licence-holder-postcode': {
-    mixins: ['input-text'],
+    mixin: ['input-text'],
     validate: ['required', 'postcode'],
     formatter: ['ukPostcode'],
     className: ['govuk-input', 'govuk-input--width-10']
@@ -63,38 +63,38 @@ module.exports = {
   },
   'premises-address-line-1': {
     validate: ['required', { type: 'maxlength', arguments: [250]}, 'notUrl'],
-    mixins: ['input-text'],
+    mixin: ['input-text'],
     className: ['govuk-input', 'govuk-!-width-two-thirds']
   },
   'premises-address-line-2': {
     validate: [{ type: 'maxlength', arguments: [250]}, 'notUrl'],
-    mixins: ['input-text'],
+    mixin: ['input-text'],
     className: ['govuk-input', 'govuk-!-width-two-thirds']
   },
   'premises-town-or-city': {
     validate: ['required', { type: 'maxlength', arguments: [250]}, 'notUrl'],
-    mixins: 'input-text',
+    mixin: 'input-text',
     className: ['govuk-input', 'govuk-!-width-two-thirds']
   },
   'premises-postcode': {
     validate: ['required', 'postcode'],
-    mixins: 'input-text',
+    mixin: 'input-text',
     formatter: ['ukPostcode'],
     className: ['govuk-input', 'govuk-input--width-10']
   },
   'premises-telephone': {
     validate: ['required'], // additional validation rules added in custom-validation.js
-    mixins: 'input-text',
+    mixin: 'input-text',
     className: ['govuk-input', 'govuk-!-width-two-thirds']
   },
   'premises-email': {
     validate: ['required', 'email'],
-    mixins: 'input-text',
+    mixin: 'input-text',
     className: ['govuk-input', 'govuk-!-width-two-thirds']
   },
   'responsible-officer-fullname': {
-    mixing: 'input-text',
-    validate: [ 'required' ],
+    mixin: 'input-text',
+    validate: [ 'required', 'notUrl' ],
     className: ['govuk-input', 'govuk-!-width-two-thirds']
   },
   'responsible-officer-email': {
@@ -108,12 +108,12 @@ module.exports = {
   },
   'responsible-officer-dbs-application-fullname': {
     mixin: 'input-text',
-    validate: [ 'required' ],
+    validate: [ 'required', 'notUrl' ],
     className: ['govuk-input', 'govuk-!-width-two-thirds']
   },
   'responsible-officer-dbs-reference': {
     mixin: 'input-text',
-    validate: [ 'required' ],
+    validate: [ 'required', 'notUrl' ],
     className: ['govuk-input', 'govuk-!-width-two-thirds']
   },
   'responsible-officer-dbs-date-of-issue': dateComponent('responsible-officer-dbs-date-of-issue', {
