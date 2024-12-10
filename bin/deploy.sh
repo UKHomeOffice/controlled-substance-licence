3d71cd3f19c833782a55fa9cc6e4743b6a69932e
Jamie Carter <137879919+jamiecarterHO@users.noreply.github.com>
2024-12-10 15:28:54 +0000

    Update deploy.sh to fix file-vault certificate (#32)
    * Update deploy.sh
    Make file-vault deployments happen before app deployments
    Make file-vault ingresses run before file-vault deployments
    
    * process services before ingresses as this is better practice

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -25,18 +25,26 @@ export DRONE_SOURCE_BRANCH=$(echo $DRONE_SOURCE_BRANCH | tr '[:upper:]' '[:lower
 
 if [[ ${KUBE_NAMESPACE} == ${BRANCH_ENV} ]]; then
   $kd -f kube/configmaps -f kube/certs
-  $kd -f kube/redis -f kube/app -f kube/file-vault
+  $kd -f kube/redis -f kube/file-vault -f kube/app
 elif [[ ${KUBE_NAMESPACE} == ${UAT_ENV} ]]; then
   $kd -f kube/configmaps/configmap.yml
-  $kd -f kube/redis -f kube/app -f kube/file-vault
+  $kd -f kube/redis
+  $kd -f kube/file-vault/file-vault-service.yml -f kube/file-vault/file-vault-ingress.yml
+  $kd -f kube/file-vault/network-policy.yml -f kube/file-vault/deployment.yml
+  $kd -f -f kube/app
 elif [[ ${KUBE_NAMESPACE} == ${STG_ENV} ]]; then
   $kd -f kube/configmaps/configmap.yml
-  $kd -f kube/redis -f kube/app -f kube/file-vault
+  $kd -f kube/redis
+  $kd -f kube/file-vault/file-vault-service.yml -f kube/file-vault/file-vault-ingress.yml
+  $kd -f kube/file-vault/network-policy.yml -f kube/file-vault/deployment.yml
+  $kd -f kube/app
 elif [[ ${KUBE_NAMESPACE} == ${PROD_ENV} ]]; then
-  $kd -f kube/configmaps/configmap.yml -f kube/app/service.yml
-  $kd -f kube/app/networkpolicy-external.yml -f kube/app/ingress-external.yml
-  $kd -f kube/redis -f kube/app/deployment.yml
-  $kd -f kube/file-vault
+  $kd -f kube/configmaps/configmap.yml
+  $kd -f kube/redis
+  $kd -f kube/file-vault/file-vault-service.yml -f kube/file-vault/file-vault-ingress.yml
+  $kd -f kube/file-vault/network-policy.yml -f kube/file-vault/deployment.yml
+  $kd -f kube/app/service.yml -f kube/app/ingress-external.yml
+  $kd -f kube/app/networkpolicy-external.yml -f kube/app/deployment.yml
 fi
 
 sleep $READY_FOR_TEST_DELAY
