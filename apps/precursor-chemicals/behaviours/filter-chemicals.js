4ef491c3761b076a2a1445dd4cb67a197aba0c70
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2024-12-18 15:27:13 +0000

    CSL-26 Refactor behaviour - do not reassign options if they did not change

--- apps/precursor-chemicals/behaviours/filter-chemicals.js
+++ apps/precursor-chemicals/behaviours/filter-chemicals.js
@@ -3,9 +3,10 @@ module.exports = superclass => class extends superclass {
     const chosenCategory = req.sessionModel.get('substance-category');
     let options = req.form.options.fields['which-chemical'].options;
     if (chosenCategory !== 'unknown') {
-      options = options.filter(option => !option.category || option.category === chosenCategory);
+      req.form.options.fields['which-chemical'].options = options.filter(
+        option => !option.category || option.category === chosenCategory
+      );
     }
-    req.form.options.fields['which-chemical'].options = options;
 
     next();
   }
