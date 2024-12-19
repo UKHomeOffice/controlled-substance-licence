e463454d36e70d359faf02b172c236c72774c372
Robert McCann <robert.mccann@digital.homeoffice.gov.uk>
2024-12-19 09:39:32 +0000

    update examples for formatDate

--- test/unit/utils.test.js
+++ test/unit/utils.test.js
@@ -15,11 +15,11 @@ describe('Utilities \'getLabel\'', () => {
 });
 
 describe('Utilities \'formatDate\'', () => {
-  test('reformats a string type date from YYYY-MM-DD to Day Month Year', () => {
+  test('reformats a string type date from YYYY-MM-DD to DD/MM/YYYY', () => {
     expect(formatDate('1987-08-14')).toBe('14 August 1987');
   });
 
-  test('reformats a string type date from MM/DD/YYYY to Day Month Year', () => {
+  test('reformats a string type date from MM/DD/YYYY to DD/MM/YYYY', () => {
     expect(formatDate('08/14/1987')).toBe('14 August 1987');
   });
 
