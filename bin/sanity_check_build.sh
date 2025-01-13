2e086d360b050bd6ff9c04f51595bd02d7b10354
dk4g <157156245+dk4g@users.noreply.github.com>
2025-01-13 17:34:11 +0000

    rename default branch;

--- bin/sanity_check_build.sh
+++ bin/sanity_check_build.sh
@@ -6,13 +6,15 @@ export BRANCH=$(drone build info $GIT_REPO $DRONE_BUILD_PARENT --format {{.Targe
 export EVENT=$(drone build info $GIT_REPO $DRONE_BUILD_PARENT --format {{.Event}})
 export REFS=$(drone build info $GIT_REPO $DRONE_BUILD_PARENT --format {{.Ref}})
 
+echo "Build number: $DRONE_BUILD_PARENT, default branch: $DRONE_REPO_BRANCH, current branch: $BRANCH, status: $STATUS"
+
 if [[ "$STATUS" != "success" ]]; then
   echo "Build number $DRONE_BUILD_PARENT failed due to unsuccessful status."
   exit 1
 fi
 
-if [[ "$BRANCH" != "main" ]]; then
-  echo "Build number $DRONE_BUILD_PARENT failed because it's not on the main branch."
+if [[ "$BRANCH" != "$DRONE_REPO_BRANCH" ]]; then
+  echo "Build number $DRONE_BUILD_PARENT failed because it's not on the $DRONE_REPO_BRANCH branch."
   exit 1
 fi
 
@@ -21,8 +23,8 @@ if [[ "$EVENT" != "push" ]]; then
   exit 1
 fi
 
-if [[ "$REFS" != "refs/heads/main" ]]; then
-  echo "Build number $DRONE_BUILD_PARENT failed because the reference is not refs/heads/main."
+if [[ "$REFS" != "refs/heads/$DRONE_REPO_BRANCH" ]]; then
+  echo "Build number $DRONE_BUILD_PARENT failed because the reference is not refs/heads/$DRONE_REPO_BRANCH."
   exit 1
 fi
 
