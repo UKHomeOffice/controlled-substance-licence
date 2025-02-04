afec5082912e3d2d65d4bf25f83c5c09afaec379
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2025-02-04 14:15:11 +0000

    CSL-60 Update custom-redirect

--- apps/common/behaviours/custom-redirect.js
+++ apps/common/behaviours/custom-redirect.js
@@ -1,3 +1,36 @@
+const checkResponsibleForSecurity = (req, formApp, currentRoute, action) => {
+  if (
+    formApp === '/controlled-drugs' &&
+    currentRoute === '/responsible-for-security' &&
+    action === 'edit' &&
+    req.form.values['responsible-for-security'] === 'same-as-managing-director'
+  ) {
+    return true;
+  } else if (
+    formApp === '/controlled-drugs' &&
+    currentRoute === '/responsible-for-security' &&
+    action === 'edit' &&
+    !!req.sessionModel.get('person-responsible-for-security-full-name')
+  ) {
+    return true;
+  }
+  return false;
+};
+
+const checkResponsibleForSecurityDetails = (req, formApp, currentRoute, action) => (
+  formApp === '/controlled-drugs' &&
+  currentRoute === '/person-responsible-for-security' &&
+  action === 'edit' &&
+  !!req.sessionModel.get('person-responsible-for-security-dbs-fullname')
+);
+
+const checkResponsibleForSecurityDbs = (req, formApp, currentRoute, action) => (
+  formApp === '/controlled-drugs' &&
+  currentRoute === '/security-officer-dbs' &&
+  action === 'edit' &&
+  !!req.sessionModel.get('person-responsible-for-security-dbs-subscription')
+);
+
 module.exports = superclass => class extends superclass {
   successHandler(req, res, next) {
     const currentRoute = req.form.options.route;
@@ -7,19 +40,14 @@ module.exports = superclass => class extends superclass {
 
     this.emit('complete', req, res);
 
-    if (
-      currentRoute === '/responsible-for-security' &&
-      action === 'edit' &&
-      formApp === '/controlled-drugs' &&
-      req.form.values['responsible-for-security'] === 'same-as-managing-director'
-    ) {
-      return res.redirect(`${formApp}${confirmStep}`);
+    switch(true) {
+      case req.sessionModel.get('referred-by-summary'):
+      case checkResponsibleForSecurity(req, formApp, currentRoute, action):
+      case checkResponsibleForSecurityDetails(req, formApp, currentRoute, action):
+      case checkResponsibleForSecurityDbs(req, formApp, currentRoute, action):
+        return res.redirect(`${formApp}${confirmStep}`);
+      default:
+        return super.successHandler(req, res, next);
     }
-
-    if (req.sessionModel.get('referred-by-summary')) {
-      return res.redirect(`${formApp}${confirmStep}`);
-    }
-
-    return super.successHandler(req, res, next);
   }
 };
--- apps/controlled-drugs/index.js
+++ apps/controlled-drugs/index.js
@@ -211,6 +211,7 @@ const steps = {
       'person-responsible-for-security-email-address',
       'person-responsible-for-security-confirmed-dbs'
     ],
+    behaviours: [CustomRedirect],
     next: '/security-officer-dbs',
     continueOnEdit: true
   },
@@ -222,6 +223,7 @@ const steps = {
       'person-responsible-for-security-dbs-reference',
       'person-responsible-for-security-dbs-date-of-issue'
     ],
+    behaviours: [CustomRedirect],
     continueOnEdit: true
   },
 
