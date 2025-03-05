1b824ba385ae7f8dd756255d9ba83b30c14ffc56
RobertMcCann <79094247+RobertMcCann@users.noreply.github.com>
2025-03-05 15:53:53 +0000

    CSL[THC]-207-low-thc-skeleton (#89)
    * adds low thc flow skeleton

---
+++ apps/industrial-hemp/index.js
@@ -0,0 +1,42 @@
+const hof = require('hof');
+const Summary = hof.components.summary;
+
+const steps = {
+
+  /** Start of journey */
+
+  '/application-type': {
+    next: '/licensee-type',
+    backLink: '/licence-type'
+  },
+
+  '/licensee-type': {
+    next: '/confirm'
+  },
+
+  /** Continue an application */
+
+
+  /** Renew existing licence - Background Information */
+
+
+  /** Existing licence apply for new site - Background Information */
+
+
+  /** First time licensee - About the applicants */
+
+  '/confirm': {
+    behaviours: [Summary],
+    sections: require('./sections/summary-data-sections')
+  }
+
+};
+
+module.exports = {
+  name: 'industrial-hemp',
+  baseUrl: '/industrial-hemp',
+  fields: 'apps/industrial-hemp/fields',
+  translations: 'apps/industrial-hemp/translations',
+  params: '/:action?/:id?/:edit?',
+  steps: steps
+};
