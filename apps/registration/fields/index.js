d06fd33721dccf52c51c9b4636658a6cbbe83a92
Robert McCann <robert.mccann@digital.homeoffice.gov.uk>
2025-03-27 16:49:17 +0000

    fix lint

--- apps/registration/fields/index.js
+++ apps/registration/fields/index.js
@@ -1,4 +1,54 @@
 
 module.exports = {
-
+  'company-name': {
+    mixin: 'input-text',
+    validate: ['required', 'notUrl', { type: 'minlength', arguments: 2 }, { type: 'maxlength', arguments: 200 }],
+    className: ['govuk-input', 'govuk-!-width-two-thirds']
+  },
+  'name-of-responsible-person': {
+    mixin: 'input-text',
+    validate: ['required', 'notUrl', { type: 'maxlength', arguments: 250 }],
+    className: ['govuk-input', 'govuk-!-width-two-thirds']
+  },
+  'company-number': {
+    mixin: 'input-text',
+    validate: [], // additional validation rules added in custom-validation.js
+    className: ['govuk-input', 'govuk-!-width-two-thirds']
+  },
+  telephone: {
+    mixin: 'input-text',
+    validate: ['required'], // additional validation covered in custom-validation.js
+    className: ['govuk-input', 'govuk-!-width-two-thirds']
+  },
+  email: {
+    mixin: 'input-text',
+    validate: ['required', 'email'],
+    className: ['govuk-input', 'govuk-!-width-two-thirds']
+  },
+  'website-url': {
+    mixin: 'input-text',
+    validate: ['url', { type: 'maxlength', arguments: 250 }],
+    className: ['govuk-input', 'govuk-!-width-two-thirds']
+  },
+  'licence-holder-address-line-1': {
+    mixin: 'input-text',
+    validate: ['required', 'notUrl', { type: 'maxlength', arguments: 250 }],
+    className: ['govuk-input', 'govuk-!-width-two-thirds']
+  },
+  'licence-holder-address-line-2': {
+    mixin: 'input-text',
+    validate: ['notUrl', { type: 'maxlength', arguments: 250 }],
+    className: ['govuk-input', 'govuk-!-width-two-thirds']
+  },
+  'licence-holder-town-or-city': {
+    mixin: 'input-text',
+    validate: ['required', 'notUrl', { type: 'maxlength', arguments: 250 }],
+    className: ['govuk-input', 'govuk-!-width-two-thirds']
+  },
+  'licence-holder-postcode': {
+    mixin: 'input-text',
+    validate: ['required', 'postcode'],
+    formatter: ['ukPostcode'],
+    className: ['govuk-input', 'govuk-input--width-10']
+  }
 };
