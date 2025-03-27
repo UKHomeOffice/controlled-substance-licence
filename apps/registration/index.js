47c65db98b71b4d967c28b1310cdd953e2a76218
Robert McCann <robert.mccann@digital.homeoffice.gov.uk>
2025-03-27 10:21:39 +0000

    add other steps to skeleton

--- apps/registration/index.js
+++ apps/registration/index.js
@@ -1,10 +1,35 @@
+const hof = require('hof');
+const Summary = hof.components.summary;
 
 const steps = {
 
   '/': {
     next: '/licence-holder-details',
     template: 'start'
-  }
+  },
+
+  '/licence-holder-details': {
+    next: '/confirm'
+  },
+
+  '/confirm': {
+    behaviours: [Summary],
+    sections: require('./sections/summary-data-sections'),
+    next: '/declaration',
+    locals: {
+      fullWidthPage: true
+    }
+  },
+
+  '/declaration': {
+    next: '/registration-submitted'
+  },
+
+  '/registration-submitted': {
+    backLink: false,
+    clearSession: true
+  },
+
 };
 
 module.exports = {
