1432e88eae78e389f1107aed1124b5a79e77f710
TemitopeAyokuHO <temitope.ayoku@digital.homeoffice.gov.uk>
2025-03-28 10:22:30 +0000

    Added fields to summary page for  CSL-141

--- apps/industrial-hemp/sections/summary-data-sections.js
+++ apps/industrial-hemp/sections/summary-data-sections.js
@@ -7,6 +7,14 @@ module.exports = {
       {
         step: '/application-type',
         field: 'amend-application-details'
+      },
+      {
+        step: '/company-number-changed',
+        field: 'is-company-ref-changed'
+      },
+      {
+        step: '/company-name-changed',
+        field: 'is-company-name-changed'
       }
     ]
   },
@@ -127,8 +135,20 @@ module.exports = {
       {
         step: '/authorised-witness-dbs-updates',
         field: 'authorised-witness-dbs-subscription'
+      },
+      {
+        step: '/why-new-licence',
+        field: 'why-new-licence'
+      },
+      {
+        step: '/when-moving-site',
+        field: 'moving-site-date',
+        parse: date => date && formatDate(new Date(date))
+      },
+      {
+        step: '/contractual-agreement',
+        field: 'is-contractual-agreement'
       }
-
     ]
   }
 };
