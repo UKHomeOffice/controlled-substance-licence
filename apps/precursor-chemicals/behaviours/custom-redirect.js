7944a8ef47de05541bb3ff511753a0cdd4657622
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2025-03-31 17:43:18 +0100

    CSL-6 Update custom-redirect for PC to handle changes from info given page

--- apps/precursor-chemicals/behaviours/custom-redirect.js
+++ apps/precursor-chemicals/behaviours/custom-redirect.js
@@ -1,3 +1,5 @@
+const { currentStepSatisfiesForkCondition } = require('../../../utils');
+
 // Handle routing around /companies-house-number and /cannot-continue
 const checkCompaniesHouseRef = (req, currentRoute) => (
   currentRoute === '/companies-house-number' &&
@@ -6,17 +8,28 @@ const checkCompaniesHouseRef = (req, currentRoute) => (
 
 module.exports = superclass => class extends superclass {
   successHandler(req, res, next) {
-    const formApp = req.baseUrl;
-    const { route: currentRoute, confirmStep } = req.form.options;
+    if (!req.form.options.ignoreCustomRedirect) {
+      const formApp = req.baseUrl;
+      const { route: currentRoute, confirmStep } = req.form.options;
+      const isContinueOnEdit = req.form.options.continueOnEdit;
 
-    this.emit('complete', req, res);
+      this.emit('complete', req, res);
 
-    if (checkCompaniesHouseRef(req, currentRoute)) {
-      return res.redirect(`${formApp}/cannot-continue`);
-    }
+      if (checkCompaniesHouseRef(req, currentRoute)) {
+        return res.redirect(`${formApp}/cannot-continue`);
+      }
 
-    if(req.sessionModel.get('referred-by-summary')) {
-      return res.redirect(`${formApp}${confirmStep}`);
+      if (req.sessionModel.get('referred-by-summary')) {
+        return res.redirect(`${formApp}${confirmStep}`);
+      }
+
+      if (
+        req.sessionModel.get('referred-by-information-given-summary') &&
+        !isContinueOnEdit &&
+        !currentStepSatisfiesForkCondition(req)
+      ) {
+        return res.redirect(`${formApp}/information-you-have-given-us`);
+      }
     }
 
     return super.successHandler(req, res, next);
