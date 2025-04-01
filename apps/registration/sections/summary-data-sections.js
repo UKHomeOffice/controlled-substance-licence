e00e646d68ba135077d81b5f754a990ea01f440d
Vinodha Samiyappan <vinodha.samiyappan@digital.homeoffice.gov.uk>
2025-04-01 09:20:59 +0100

    Added summary page sections and fields

--- apps/registration/sections/summary-data-sections.js
+++ apps/registration/sections/summary-data-sections.js
@@ -1,4 +1,55 @@
 'use strict';
 
 module.exports = {
+    'licence-holder-details': {
+    steps: [
+      {
+        step: '/licence-holder-details',
+        field: 'company-name'
+      },
+      {
+        step: '/licence-holder-details',
+        field: 'name-of-responsible-person'
+      },
+      {
+        step: '/licence-holder-details',
+        field: 'company-number',
+        parse: value => value.toUpperCase() || 'Not Provided'
+      },
+      {
+        step: '/licence-holder-details',
+        field: 'telephone'
+      },
+      {
+        step: '/licence-holder-details',
+        field: 'email'
+      },
+      {
+        step: '/licence-holder-details',
+        field: 'website-url',
+        parse: value => value || 'Not Provided'
+      }
+    ]
+  },
+  'licence-holder-address': {
+    steps: [
+      {
+        step: '/licence-holder-address',
+        field: 'licence-holder-address-line-1'
+      },
+      {
+        step: '/licence-holder-address',
+        field: 'licence-holder-address-line-2',
+        parse: value => value || 'Not Provided'
+      },
+      {
+        step: '/licence-holder-address',
+        field: 'licence-holder-town-or-city'
+      },
+      {
+        step: '/licence-holder-address',
+        field: 'licence-holder-postcode'
+      }
+    ]
+  }
 };
