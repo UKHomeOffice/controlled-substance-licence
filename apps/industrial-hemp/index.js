9213ebb932fccd5d6cfe1b88ddcd195017b3bae9
TemitopeAyokuHO <138882912+TemitopeAyokuHO@users.noreply.github.com>
2025-04-01 13:52:57 +0100

    Update index.js

--- apps/industrial-hemp/index.js
+++ apps/industrial-hemp/index.js
@@ -116,7 +116,9 @@ const steps = {
     next: '/licence-holder-details'
   },
 
-  '/when-contract-start': {},
+  '/when-contract-start': {
+    next: '/licence-holder-details'
+  },
 
   /** First time licensee - About the applicants */
 
