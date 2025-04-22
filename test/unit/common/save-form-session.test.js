53ed47889cccfb0c7972cbeeb2515bac2805a8f4
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2025-04-22 14:05:11 +0100

    refactor save-form-session and unit tests

--- test/unit/common/save-form-session.test.js
+++ test/unit/common/save-form-session.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 const Behaviour = require('../../../apps/common/behaviours/save-form-session');
 const reqres = require('hof').utils.reqres;
 const Model = require('hof/model');
@@ -119,27 +120,28 @@ describe('save-form-session', () => {
       expect(req.sessionModel.set).toHaveBeenCalledWith('application-id', 1);
     });
 
-    test('calls HOFs _request method with the expected values when an application-id exists', async () => {
+    test('calls _request expected values if application-id exists and user will continue previous application', async () => {
       mockSessionAttributes['application-id'] = 1;
+      mockSessionAttributes['application-form-type'] = 'continue-an-application';
       await instance.successHandler(req, res, next);
       expect(mockRequest).toHaveBeenCalledWith({
         url: 'http://127.0.0.1:5000/applications/1',
         method: 'PATCH',
         data: { session: mockSessionAttributes }
       });
+      expect(req.log).not.toHaveBeenCalledWith('info', 'Overwriting saved application: 1');
     });
 
-    test('calls _request with expected values if application-id and overwrite-application flag is set', async () => {
+    test('calls _request with expected values if application-id exists but user will not continue previous application', async () => {
       jest.spyOn(Date.prototype, 'toISOString').mockReturnValue('2025-02-23T00:00:00.000Z');
       mockSessionAttributes['application-id'] = 1;
-      mockSessionAttributes['overwrite-application'] = true;
       await instance.successHandler(req, res, next);
       expect(mockRequest).toHaveBeenCalledWith({
         url: 'http://127.0.0.1:5000/applications/1',
         method: 'PATCH',
         data: { session: mockSessionAttributes, created_at: '2025-02-23T00:00:00.000Z' }
       });
-      expect(req.sessionModel.unset).toHaveBeenCalledWith('overwrite-application');
+      expect(req.log).toHaveBeenCalledWith('info', 'Overwriting saved application: 1');
     });
 
     test('does not cause saving behaviour if the current path is in the exemption list', async () => {
