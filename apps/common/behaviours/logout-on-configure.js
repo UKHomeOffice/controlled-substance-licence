cf0768ba4704728c42ad0a72f479b8a721322a0b
dk4g <157156245+dk4g@users.noreply.github.com>
2025-05-01 16:15:22 +0100

    enabled logout process on save and exit flow;

--- apps/common/behaviours/logout-on-configure.js
+++ apps/common/behaviours/logout-on-configure.js
@@ -1,15 +1,15 @@
 'use strict';
 
-// const auth = require('../../../../utils/auth');
-// const { resetAllSessions } = require('../../../../utils');
+const auth = require('../../../../utils/auth');
+const { resetAllSessions } = require('../../../../utils');
 
 module.exports = superclass => class extends superclass {
   configure(req, res, next) {
-    // auth.setReq(req);
-    // auth.logout();
+    auth.setReq(req);
+    auth.logout();
 
     req.log('info', 'Clear session on sign out');
-    // resetAllSessions(req);
+    resetAllSessions(req);
 
     return super.configure(req, res, next);
   }
