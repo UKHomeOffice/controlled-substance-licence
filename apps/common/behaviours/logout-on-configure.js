58e0240fd0564977eb4d8ecccf80a2aca123b809
dk4g <157156245+dk4g@users.noreply.github.com>
2025-05-01 16:57:48 +0100

    logout at render;

--- apps/common/behaviours/logout-on-configure.js
+++ apps/common/behaviours/logout-on-configure.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const auth = require('../../../../utils/auth');
-const { resetAllSessions } = require('../../../../utils');
+const auth = require('../../../utils/auth');
+const { resetAllSessions } = require('../../../utils');
 
 module.exports = superclass => class extends superclass {
-  configure(req, res, next) {
+  render(req, res, next) {
     auth.setReq(req);
     auth.logout();
 
     req.log('info', 'Clear session on sign out');
     resetAllSessions(req);
 
-    return super.configure(req, res, next);
+    return super.render(req, res, next);
   }
 };
