463dfdbb94d47b8624265889e4625349eb8747f8
Robert McCann <robert.mccann@digital.homeoffice.gov.uk>
2025-05-14 16:09:21 +0100

    updates from CSL-174 and figma changes

--- apps/registration/fields/index.js
+++ apps/registration/fields/index.js
@@ -269,7 +269,7 @@ module.exports = {
       'required',
       'date',
       { type: 'before', arguments: ['0', 'days'] },
-      { type: 'after', arguments: ['100', 'years'] } // Validate the date to be less than 100 years in the past
+      { type: 'after', arguments: ['2002-12-31'] }
     ],
     legend: {
       className: 'govuk-!-margin-bottom-4'
