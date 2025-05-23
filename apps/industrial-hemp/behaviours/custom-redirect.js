94ab7e45af77eec469ecd4dc69dd79772804d9e4
Vinodha Samiyappan <155738658+vinodhasamiyappanHO@users.noreply.github.com>
2025-05-23 12:48:29 +0100

    CSL - 213 - THC - Added Information you given us page and save and exit feature (#157)
    * Added Information you given us page and save and exit feature in Low THC form
    
    * Adding header for /information-you-have-given-us page

--- apps/industrial-hemp/behaviours/custom-redirect.js
+++ apps/industrial-hemp/behaviours/custom-redirect.js
@@ -1,3 +1,5 @@
+const { findSatisfiedForkCondition } = require('../../../utils');
+
 // Handle routing around /companies-house-number and /cannot-continue
 const checkCompaniesHouseRef = (req, currentRoute) => (
   currentRoute === '/company-number-changed' &&
@@ -8,11 +10,19 @@ module.exports = superclass => class extends superclass {
   successHandler(req, res, next) {
     const formApp = req.baseUrl;
     const { route: currentRoute, confirmStep } = req.form.options;
+    const isContinueOnEdit = req.form.options.continueOnEdit;
 
     this.emit('complete', req, res);
 
     if (checkCompaniesHouseRef(req, currentRoute)) {
       return res.redirect(`${formApp}/register-again`);
+    }
+
+    if (req.sessionModel.get('referred-by-information-given-summary') &&
+        !findSatisfiedForkCondition(req, req.form.options.forks)) {
+      if (!isContinueOnEdit) {
+        return res.redirect(`${formApp}/information-you-have-given-us`);
+      }
     }
 
     if(req.sessionModel.get('referred-by-summary')) {
