239c03d3e70d4a6f38d4f63fc2c8da9ad6bef8a6
TemitopeAyokuHO <temitope.ayoku@digital.homeoffice.gov.uk>
2025-05-29 01:23:36 +0100

    feedback review

--- utils/pass-generator.js
+++ utils/pass-generator.js
@@ -1,11 +1,48 @@
 'use strict';
+const config = require('../config');
+const logger = require('hof/lib/logger')({ env: config.env });
 
-const cryptoRandomString = require('crypto-random-string');
+// At least one uppercase, one lowercase, one special character, and one digit, and minimum 8 characters
+const passwordPolicyRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[!#$%&*+=?@])(?=.*\d)[A-Za-z!#$%&*+=?@0-9]{8,}$/;
 
-const generatePass = length => {
-  return cryptoRandomString({length: length, type: 'ascii-printable'});
+/**
+ * Check if passward value is valid
+ *
+ * @param {string} value - random generated value from crypto-random-string
+ * @returns {bool}
+ */
+const validatePassword = value => passwordPolicyRegex.test(value);
+
+/**
+ * Generate a password with 8 characters length and should meet requirement
+ * - The password must be created with a minimum of 8 characters.
+ * - The characters must consist of at least 1 uppercase character.
+ * - The characters must consist of at least 1 lowercase character.
+ * - The characters must consist of at least 1 digit
+ * @param {string} length - expected password length.
+ * @param {string} characterType - expected character type example('ABabc1234').
+ * @returns {string} -  valid password that meets password policy
+*/
+const generatePassword =  async (length, characterType) => {
+  let randomPassword = '';
+  let isPolicyMatch;
+  try {
+    const cryptoRandomString = await import('crypto-random-string');
+    do{
+      randomPassword = cryptoRandomString.default({length: length, characters: characterType});
+      isPolicyMatch = validatePassword(randomPassword);
+    }
+    while(!isPolicyMatch);
+  }catch(error) {
+    const errorMessage = `Failed to generate password: ${error}`;
+    logger.log('error', errorMessage);
+    throw new Error(errorMessage);
+  }
+  return randomPassword;
 };
 
+
 module.exports = {
-  generatePass
+  generatePassword,
+  validatePassword
 };
