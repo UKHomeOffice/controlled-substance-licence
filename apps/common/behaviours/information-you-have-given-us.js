1d162ffef20284ed2f9054205da53724d6ebe127
Vinodha Samiyappan <155738658+vinodhasamiyappanHO@users.noreply.github.com>
2025-10-29 14:39:54 +0000

    CSL-330 - Backward compatibility logic for draft application  (#220)
    * updated logic to define the nextUnsavedStep;
    
    * Updated unit tests;
    
    * witnessDetailsLimit increased to 15;
    
    * updated page title tag for witness-dbs-information step;
    
    ---------
    
    Co-authored-by: dk4g <157156245+dk4g@users.noreply.github.com>

--- apps/common/behaviours/information-you-have-given-us.js
+++ apps/common/behaviours/information-you-have-given-us.js
@@ -23,24 +23,16 @@ module.exports = superclass => class extends superclass {
   getValues(req, res, next) {
     req.sessionModel.set('referred-by-information-given-summary', true);
 
-    // steps in the session fall out of sync when changed from the current progress report page
-    // this reorders them to ensure the user jumps to the last step they filled out
     const sessionSteps = req.sessionModel.get('steps');
     if (!sessionSteps.includes('/information-you-have-given-us')) {
       sessionSteps.push('/information-you-have-given-us');
     }
-    const stepJourneyFromValues = buildStepJourneyFromSessionValues(req);
-    const visitedFormSteps = stepJourneyFromValues.filter(step => sessionSteps.includes(step));
-    req.sessionModel.set('steps', visitedFormSteps);
 
-    let nextStep;
-    const { confirmStep } = req.form.options;
-    if (visitedFormSteps.includes(confirmStep)) {
-      nextStep = confirmStep;
-    } else {
-      const lastVisitedStep = visitedFormSteps[visitedFormSteps.length - 1];
-      nextStep = stepJourneyFromValues[stepJourneyFromValues.findIndex(item => item === lastVisitedStep) + 1];
-    }
+    const stepJourneyFromValues = buildStepJourneyFromSessionValues(req);
+
+    const nextStep = stepJourneyFromValues.find(step => !sessionSteps.includes(step)) ||
+                    req.form.options.confirmStep;
+
     req.sessionModel.set('save-return-next-step', nextStep);
 
     return super.getValues(req, res, next);
@@ -55,6 +47,7 @@ module.exports = superclass => class extends superclass {
     }
 
     const nextUnsavedStep = req.sessionModel.get('save-return-next-step');
+
     if (nextUnsavedStep) {
       return res.redirect(`${formApp}${nextUnsavedStep}`);
     }
