2dfd26a723809412e0aa991f8393952f5e93c266
Robert McCann <79094247+RobertMcCann@users.noreply.github.com>
2026-07-15 15:00:27 +0100

    add file vault branch url printed in drone as dev improvement

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -76,5 +76,6 @@ sleep $READY_FOR_TEST_DELAY
 
 if [[ ${KUBE_NAMESPACE} == ${BRANCH_ENV} ]]; then
   echo "Branch - $APP_NAME-$DRONE_SOURCE_BRANCH.internal.branch.sas-notprod.homeoffice.gov.uk"
+  echo "File Vault Branch - fv-$DRONE_SOURCE_BRANCH.branch.sas-notprod.homeoffice.gov.uk"
 fi
 
